600b7b3fe98b76b8caf9ba99ce6a9d3bf1ca3435
AngryFalcon89 <ahmadbilal.199822@gmail.com>
2024-01-09 17:18:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/composenavigation/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.composenavigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composenavigation.ui.theme.ComposeNavigationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeNavigationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = "home"
+                    ){
+                        composable("home"){
+                            HomeScreen(navController)
+                        }
+                        composable("second"){
+                            SecondScreen(navController)
+                        }
+                        composable("last"){
+                            LastScreen(navController)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
